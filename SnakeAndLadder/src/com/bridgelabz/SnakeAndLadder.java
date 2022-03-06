4c0c908b1f0a92b4f7a9552f7cfb728bec405364
csumn <csumn13@gmail.com>
2022-03-06 23:58:43 +0530

    Option check file addeed

--- SnakeAndLadder/src/com/bridgelabz/SnakeAndLadder.java
+++ SnakeAndLadder/src/com/bridgelabz/SnakeAndLadder.java
@@ -1,8 +1,30 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
+	public static final int NO_PLAY = 1;
+	public static final int LADDER = 2;
+	public static final int SNAKE =3;
+
 	public static void main(String[] args) {
-	int dice_number = (int) Math.floor(Math.random() *6+1);
-	System.out.println("Dice Number is: " +dice_number);
+		int position = 0;
+		System.out.println("First player is at postion "+position);
+		
+		Random random = new Random();
+		int dieNum = random.nextInt(7-1)+1;
+		System.out.println("Dice number : "+dieNum);
+		
+		int choice = random.nextInt(4-1)+1;
+		
+		if(choice == NO_PLAY){
+			System.out.println("No Play");
+		}else if(choice == LADDER) {
+			System.out.println("Player inclined by "+dieNum);
+			position+=dieNum;
+		}else if(choice == SNAKE) {
+			System.out.println("Player declined by "+dieNum);
+			position-=dieNum;
+		}		
 	}
 }
